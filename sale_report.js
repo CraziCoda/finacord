3f121de77af907898a54ecf5930a6df5b127c1d1
CraziCoda <64246414+CraziCoda@users.noreply.github.com>
2020-08-08 08:01:14 +0000

    Update sale_report.js

--- sale_report.js
+++ sale_report.js
@@ -12,7 +12,6 @@ request.onerror = (event) => {
     console.log("Error: " + event);
 };
 
-
 request.onupgradeneeded = (event) => {
     console.log('Hello');
     db = event.target.result;
@@ -20,23 +19,33 @@ request.onupgradeneeded = (event) => {
 request.onsuccess = (event) => {
     db = request.result;
     console.log("Success: " + db);
-    view();
+    createPreview();
 };
-let view = () => {
-    console.log("Hmm" + db);
+
+function createPreview() {
+    var row = document.getElementById("preview_form");
     let objectStore = db.transaction("preview").objectStore("preview");
-    let sale_report = document.getElementById("sale_report");
+
 
     objectStore.openCursor().onsuccess = (event) => {
         let cursor = event.target.result;
         if (cursor) {
-            console.log(cursor);
-            //Codde for DOM manipulation goes here it shouldn't be under the cursor.continue
-            //no need for loops this function is going to iterate over elements
-            //To get a value use cursor.value.nameoftheobject
+            var newRow = row.insertRow(1);
+            //inserting cells per each input
+            var cell1 = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+            var cell3 = newRow.insertCell(2);
+            var cell4 = newRow.insertCell(3);
+            var cell5 = newRow.insertCell(4);
+
+            cell1.innerHTML = cursor.value.quantity;
+            cell2.innerHTML = cursor.value.unit;
+            cell3.innerHTML = cursor.value.product;
+            cell4.innerHTML = cursor.value.payment;
+            cell5.innerHTML = cursor.value.total;
+
             cursor.continue();
         }
-
     };
 
-};
+}
